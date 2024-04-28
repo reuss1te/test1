e3d29a6c7b1bbaac775e5b1f4b21aebb741ae453
reuss1te <95479217+reuss1te@users.noreply.github.com>
2024-04-28 12:30:27 +0400

    Add files via upload

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+void drawTriangle(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            std::cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "ru_RU");
+    int n;
+    std::cout << "Введите высоту треугольника: ";
+    std::cin >> n;
+
+    drawTriangle(n);
+
+    return 0;
+}
